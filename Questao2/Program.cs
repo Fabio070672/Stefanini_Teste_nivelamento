7f71cbd5bf1cbbe1b5b35db6a2a8e49c1178283c
fabio martins <fabioomartins@gmail.com>
2024-03-05 17:00:39 -0300

    Alterando arquivos de resposta

--- Questao2/Program.cs
+++ Questao2/Program.cs
@@ -21,10 +21,7 @@ public class Program
         totalGoals = await GetTotalScoredGoals(teamName, year);
 
         Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
-
-        // Output expected:
-        // Team Paris Saint - Germain scored 109 goals in 2013
-        // Team Chelsea scored 92 goals in 2014
+              
     }
 
     private static async Task<int> GetTotalScoredGoals(string team, int year)
